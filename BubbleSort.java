6380339926742ee1e772d77762d0b54cee559e30
jiho <wlgj0802@naver.com>
2023-04-18 19:56:02 +0900

    test

---
+++ BubbleSort.java
@@ -0,0 +1,16 @@
+public class BubbleSort {
+    public static void bubble(int[] arr) {
+        int temp = 0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=1; i<arr.length-i; j++){
+                if(arr[j-1]>arr[j]){
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+            
+        
+    }
+}
