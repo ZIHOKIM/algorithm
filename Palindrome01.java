796771f2a71559e2f14eff7f8fb8bd35aab36270
ZIHOKIM <wlgj0802@naver.com>
2023-06-02 17:43:25 +0900

    String-00107

---
+++ Palindrome01.java
@@ -0,0 +1,22 @@
+public class Palindrome01 {
+    public String solution(String str){
+        String answer = "YES";
+        str = str.toUpperCase();
+        int len = str.length();
+        for(int i=0; i<len/2; i++){
+            if(str.charAt(i)!= str.charAt(len-1-i)) return "NO";
+        }
+        return answer;
+
+    }
+//StringBuilder사용
+    public String solution2(String str){
+        String answer = "NO";
+        String tmp = new StringBuilder(str).reverse().toString();
+        if(str.equalsIgnoreCase(tmp)) return "YES";
+        return answer;
+    }
+
+
+
+}
