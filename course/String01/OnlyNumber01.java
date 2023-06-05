faccfb2bf2fe6d2e5f23ce4e52cf1d22de3ade9b
ZIHOKIM <wlgj0802@naver.com>
2023-06-05 15:26:24 +0900

    .

---
+++ course/String01/OnlyNumber01.java
@@ -0,0 +1,19 @@
+package course.String01;
+
+public class OnlyNumber01 {
+    public int solution(String s){
+        int answer =0;
+        //ASKII NUMBER 문자형 숫자의 경우 '0'~'9' -> 48~57
+        //answer * 10 +(x-48)
+        int askiiZero = 48;
+        int askiiNine = 57;
+        for(char c:s.toCharArray()){
+            if(c>=askiiZero && c<=askiiNine) answer = answer*10+(c-48);
+            //answer을 String으로 받을 경우
+            //if(Character.isDigit(x)) answer +=c;
+            //Integer.parseInt(answer);
+        }
+
+        return answer;
+    }
+}
