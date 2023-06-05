15d2d83ebb1722f0ca1ba52c73b8bb1eff7d2b81
ZIHOKIM <wlgj0802@naver.com>
2023-06-05 16:07:07 +0900

    .

---
+++ course/String01/StringDistance01.java
@@ -0,0 +1,31 @@
+package course.String01;
+
+public class StringDistance01 {
+    public  int[] solution(String s, char t) {
+        int p = 1000;
+        int[] answer = new int[s.length()];
+        for(int i = 0 ; i<s.length(); i++){
+            if(s.charAt(i)==t){
+                p=0;
+                answer[i] = p;
+            }else {
+                p++;
+                answer[i] = p;
+            }
+        }
+        p=1000;
+        //역방향으로 for문
+        for(int i =s.length()-1;i>=0;i--){
+            if(s.charAt(i)==t) p=0;
+            else {
+                p++;
+                //기존 배열값과 비교했을때 더작은 숫자로 바꿔줌
+                answer[i] = Math.min(answer[i],p);
+            }
+        }
+
+    return answer;
+
+
+    }
+}
