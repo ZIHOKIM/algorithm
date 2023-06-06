5e65c3e8dcd7ca671e94aec4cd348df191cbc240
ZIHOKIM <wlgj0802@naver.com>
2023-06-07 00:02:53 +0900

    .

---
+++ course/String01/Code12.java
@@ -0,0 +1,17 @@
+package course.String01;
+
+public class Code12 {
+    public String solution(String code, int num) {
+        String answer = "";
+
+        for(int i=1;i<num;i++){
+            String tmp = code.substring(0,7).replace('#','1').replace('*','0');
+            //이진수로 변환
+            int n = Integer.parseInt(tmp,2);
+            //숫자->문자형변환 String출력
+            answer += (char)num;
+            code = code.substring(7);
+        }
+        return answer;
+    }
+}
